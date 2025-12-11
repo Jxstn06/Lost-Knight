da72b05bbee49ef80ae42a110471a07beebf9502
Jxstn06 <s.justin2006.08@gmail.com>
2025-12-12 00:16:31 +0100

    idk

--- lostknight.py
+++ lostknight.py
@@ -3,6 +3,7 @@ from settings import Settings
 from manager import Manager
 from Szenen.menu_szene import MenuScene
 from Szenen.button import Button
+from Szenen.basis_szene import Scene
 
 
 class LostKnight:
@@ -12,13 +13,12 @@ class LostKnight:
         self.screen = pygame.display.set_mode((self.settings.screen_breite, self.settings.screen_hoehe))
         self.clock = pygame.time.Clock()
 
-        self.manager = Manager('start')
-        self.start = MenuScene(self.screen, self.manager)
+        self.manager = Manager('menuszene')
+        self.menuszene = MenuScene(self.screen, self.manager)
         self.level = Level(self.screen, self.manager)
 
-        self.states = {'start': self.start,
-                       'level': self.level
-                        }
+        self.states = {'menuszene': self.menuszene,
+                       'level': self.level}
 
     def run(self):
         while True:
@@ -27,28 +27,18 @@ class LostKnight:
                     pygame.quit()
                 # Event wird an Szene weitergegeben
                 self.states[self.manager.get_state()].handle_events(event)
-
+            # Läuft die aktuelle Szene
             self.states[self.manager.get_state()].run()
             pygame.display.update()
             self.clock.tick(self.settings.fps)
 
 
-class Level:
+# Test für den Szenen wechsel
+class Level(Scene):
     def __init__(self, display, manager):
+        super().__init__()
         self.display = display
         self.manager = manager
-
-    def handle_events(self, event):
-        pass
 
     def run(self):
         self.display.fill('blue')
-
-
-class Start:
-    def __init__(self, display, manager):
-        self.display = display
-        self.manager = manager
-
-    def run(self):
-        self.display.fill('red')
